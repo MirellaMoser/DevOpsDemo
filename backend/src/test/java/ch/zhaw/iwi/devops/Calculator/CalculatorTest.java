337df8417c6a0e2286a57a163994693416e24269
MirellaMoser <mirella.moser@gmx.net>
2023-05-26 16:25:38 +0200

    verbesserter calculator code

--- backend/src/test/java/ch/zhaw/iwi/devops/Calculator/CalculatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/Calculator/CalculatorTest.java
@@ -7,21 +7,20 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(4, calculator.add(2, 3));
+        assertEquals(5, calculator.add(2, 3)); // Fixed expected value
         // add more test cases
     }
     
     @Test
     public void testSubtract() {
         Calculator calculator = new Calculator();
-        assertEquals(1, calculator.subtract(5, 3));
+        assertEquals(2, calculator.subtract(5, 3)); // Fixed expected value
     }
     
-    // incomplete test cases for multiply and divide methods
     @Test
     public void testMultiply() {
         Calculator calculator = new Calculator();
-        assertEquals(4, calculator.multiply(0, 5));
+        assertEquals(0, calculator.multiply(0, 5)); // Fixed expected value
         assertEquals(6, calculator.multiply(2, 3));
         assertEquals(-15, calculator.multiply(3, -5));
     }
@@ -30,21 +29,22 @@ public class CalculatorTest {
     public void testDivide() {
         Calculator calculator = new Calculator();
         assertEquals(0, calculator.divide(0, 5));
-        assertEquals(5, calculator.divide(6, 3));
+        assertEquals(2, calculator.divide(6, 3)); // Fixed expected value
         assertEquals(-3, calculator.divide(15, -5));
     }
+    
     @Test
     public void testDivideByZero() {
         Calculator calculator = new Calculator();
         assertThrows(IllegalArgumentException.class, () -> calculator.divide(6, 0));
-    // add more test cases
+        // add more test cases
     }
 
     @Test
     public void testDivide2() {
         Calculator calculator = new Calculator();
         assertEquals(0, calculator.divide(0, 5));
-        assertEquals(3, calculator.divide(6, 3));
+        assertEquals(2, calculator.divide(6, 3)); // Fixed expected value
         assertEquals(-3, calculator.divide(15, -5));
     }   
 }
