18ad6c3ead5c082efc76163c05b862d772a11f2c
MirellaMoser <mirella.moser@gmx.net>
2023-05-29 19:06:43 +0200

    changes gradlew and calculator java

--- backend/src/test/java/ch/zhaw/iwi/devops/Calculator/CalculatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/Calculator/CalculatorTest.java
@@ -7,44 +7,29 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3)); // Fixed expected value
+        assertEquals(5, calculator.add(2, 3));
         // add more test cases
     }
     
     @Test
     public void testSubtract() {
         Calculator calculator = new Calculator();
-        assertEquals(2, calculator.subtract(0, 0)); // Fixed expected value
+        assertEquals(0, calculator.subtract(0, 0));
     }
     
     @Test
     public void testMultiply() {
         Calculator calculator = new Calculator();
-        assertEquals(0, calculator.multiply(0, 5)); // Fixed expected value
+        assertEquals(0, calculator.multiply(0, 5));
         assertEquals(6, calculator.multiply(2, 3));
         assertEquals(-15, calculator.multiply(3, -5));
-    }
-    
-    @Test
-    public void testDivide() {
-        Calculator calculator = new Calculator();
-        assertEquals(0, calculator.divide(0, 5));
-        assertEquals(2, calculator.divide(1, 3)); // Fixed expected value
-        assertEquals(-3, calculator.divide(15, -5));
-    }
-    
-    @Test
-    public void testDivideByZero() {
-        Calculator calculator = new Calculator();
-        assertThrows(IllegalArgumentException.class, () -> calculator.divide(6, 0));
-        // add more test cases
     }
 
     @Test
     public void testDivide2() {
         Calculator calculator = new Calculator();
         assertEquals(0, calculator.divide(0, 5));
-        assertEquals(2, calculator.divide(3, 3)); // Fixed expected value
+        assertEquals(1, calculator.divide(3, 3));
         assertEquals(-3, calculator.divide(15, -5));
     }   
 }
